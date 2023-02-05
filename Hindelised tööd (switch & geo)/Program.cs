97a27f58ee93b6f2082f4ed8a936947f0ffc0d34
Gevin Šestak <gevin.sestak@hotmail.com>
2023-02-05 16:53:48 +0200

    Tööd

---
+++ Hindelised tööd (switch & geo)/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Drawing;
+using System.Security.Principal;
+
+namespace HindelineTöö
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta kujund: ");
+            Console.WriteLine("1. Ruut");
+            Console.WriteLine("2. Teemant");
+            Console.WriteLine("3. Ristkülik");
+            Console.WriteLine("4. Kolmnurk");
+
+            string tehe = Console.ReadLine();
+
+            switch (tehe)
+            {
+                case "1":
+                    Square();
+                    break;
+                case "2":
+                    Diamond();
+                    break;
+                case "3":
+                    Rectangle();
+                    break;
+                case "4":
+                    Triangle();
+                    break;
+                default:
+                    Console.WriteLine("Kujundit ei valitud");
+                    break;
+            }
+        }
+        private static void Square()
+        {
+            Console.WriteLine("Ruut");
+            Console.WriteLine("Sisesta ruudu külje pikkus: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Pindala:" + size * size);
+            Console.WriteLine("Ümbermõõt:" + 4 * size);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        private static void Rectangle()
+        {
+            Console.WriteLine("Sisestage ristküliku laius: ");
+            int width = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sisestage ristküliku kõrgus: ");
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ümbermõõt: " + 2 * (width + height));
+            Console.WriteLine("Pindala: " + width * height);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        private static void Diamond()
+        {
+            Console.WriteLine("Teemant");
+            Console.WriteLine("Sisesta läbimõõt: ");
+            int row = Convert.ToInt32(Console.ReadLine());
+            int j;
+            Console.WriteLine("Pindala: " + (Math.PI * (row / 2) * (row / 2)) / 2);
+            Console.WriteLine("Ümbermõõt: " + Math.PI * row);
+            for (int i = 0; i <= row; i++)
+            {
+                for (j = 1; j <= row - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            for (int i = row - 1; i >= 1; i--)
+            {
+                for (j = 1; j <= row - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+        }
+        private static void Triangle()
+        {
+            Console.WriteLine("Sisesta kolmnurga alus:");
+            double triangleBase = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Sisesta kõrgus:");
+            double triangleHeight = Convert.ToDouble(Console.ReadLine());
+            double areaOfTriangle = (triangleBase * triangleHeight) / 2;
+            Console.WriteLine("Pindala: " + areaOfTriangle);
+            for (int i = 0; i <= triangleHeight; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ Hindelised tööd (switch & geo)/Program2.cs
@@ -0,0 +1,41 @@
+﻿namespace WeekdayEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Number ja nädalapäev";
+            Console.WriteLine("Sisesta number 1-7ni, et teada saada nädalapäev");
+
+            int weekDay = int.Parse(Console.ReadLine());
+            switch (weekDay) {
+                    case 1:
+                        Console.WriteLine("Esimene päev nädalas on esmaspäev. Haigusleht äkki?");
+                        break;
+                    case 2:
+                        Console.WriteLine("Teine päev nädalas on teisipäev. Peaaegu nädala selgroo murtud.");
+                        break;
+                    case 3:
+                        Console.WriteLine("Kolmas päev nädalas on kolmapäev. Tartus tudengid juba peavad pidu.");
+                        break;
+                    case 4:
+                        Console.WriteLine("Neljas päev nädalas on neljapäev. Homme saab niisutada kurku!");
+                        break;
+                    case 5:
+                        Console.WriteLine("Viies päev nädalas on reede. PARTY!!!");
+                        break;
+                    case 6:
+                        Console.WriteLine("Kuues päev nädalas on laupäev. Pohmakas vol 1");
+                        break;
+                    case 7:
+                        Console.WriteLine("Seitsmes päev nädalas on pühapäev. Valmistu tööks!");
+                        break;
+                default:
+                    Console.Beep();
+                    Console.WriteLine("ERROR: Sisesta arv 1-7ni!");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
